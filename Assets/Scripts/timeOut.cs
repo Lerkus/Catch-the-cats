5b2b95471956d7ebe5693b6fe281b5023ffb87a3
Lerkus <Sebastian-Maag@gmx.de>
2017-03-11 16:41:13 +0100

    tutorial text hinzugefügt

---
+++ Assets/Scripts/timeOut.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timeOut : MonoBehaviour {
+
+    private Coroutine bla;
+
+	void Start () {
+        bla = StartCoroutine(da());
+	}
+
+    public IEnumerator da()
+    {
+        yield return new WaitForSeconds(15);
+        gameObject.SetActive(false);
+        StopCoroutine(bla);
+    }
+}
